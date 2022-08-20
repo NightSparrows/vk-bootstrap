4ef2a9abf5dd654726196feaa4bd8f0497e73304
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2022-08-20 18:01:19 +0800

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "VKBootstrap"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	includedirs {
+		"%{IncludeDir.VulkanSDK}"
+	}
+
+	files {
+		"src/VKBootstrap.h",
+		"src/VKBootstrap.cpp"
+	}
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
