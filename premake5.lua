8f5bb3547a1fdc68ede039764001f5a12f82fd0b
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2022-08-20 21:31:59 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "VKBootstrap"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
